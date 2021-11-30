5f2c7132c3d464115101123aff3d028ca25ce941
aliahmad8701 <aliahmadshakil@gmail.com>
2021-11-30 12:35:21 +0100

    files added to repo

---
+++ src/test/java/project/NewsletterStepDefs.java
@@ -0,0 +1,28 @@
+package project;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.Optional;
+
+import static org.junit.Assert.fail;
+
+public class NewsletterStepDefs extends AbstractStepDefs {
+
+
+    @When("the Newsletter button is clicked")
+    public void theNewsletterButtonIsClicked() {
+        homePage.clickNewsLetterButton();
+    }
+
+    @Then("the {string} newsletter error message is shown")
+    public void theNewsletterErrorMessageIsShown(String msg) {
+        Optional<String> errorMessage = homePage.getNewsletterError();
+        if (errorMessage.isPresent()) {
+            Assert.assertEquals(msg, errorMessage.get());
+        } else {
+            fail();
+        }
+    }
+}
